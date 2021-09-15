51f23e4d2f60db5feb542df94e4a6774869cfe5e
abizmo <abizmo@gmail.com>
2021-09-15 22:01:27 +0100

    Using history to simulate login

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import AppRouter from './routers/AppRouter'
+import './styles/index.css'
 
 const HeroesApp = () => {
   return (
-    <div>
+    <>
       <AppRouter />
-    </div>
+    </>
   )
 }
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const LoginPage = () => {
+const LoginPage = ({ history }) => {
+  const handleLogin = () => {
+    history.replace("/")
+  }
+
   return (
-    <div>
-      LoginPage
+    <div className="login text-center">
+      <main className="form-signin">
+        <form>
+          <h1 className="h3 mb-3 fw-normal">Please login</h1>
+          <button
+            className="w-100 btn btn-lg btn-primary"
+            onClick={handleLogin}
+            type="submit"
+          >Login</button>
+          <p className="mt-5 mb-3 text-muted">&copy; 2021</p>
+        </form>
+      </main>
     </div>
   )
 }
